b716d15ba06c45c1ff975c67814d6b02ad190f24
vak101995 <vak101995@gmail.com>
2016-11-23 12:24:39 +0530

    Add a simple output change

--- Floyd/src/com/floyd/barber/Eclipse.java
+++ Floyd/src/com/floyd/barber/Eclipse.java
@@ -5,6 +5,8 @@ public class Eclipse {
     public static void main(String[] args) {
         // TODO Auto-generated method stub
         System.out.println("See you on the dark side of the moon");
+        
+        System.out.println("Have a Cigar");
     }
 
 }
