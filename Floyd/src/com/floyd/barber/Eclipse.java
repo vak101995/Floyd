7b32b6c117fcc657a8d1317de10475a73c84adb8
vak101995 <vak101995@gmail.com>
2016-11-23 11:41:40 +0530

    Initial check in of Floyd

---
+++ Floyd/src/com/floyd/barber/Eclipse.java
@@ -0,0 +1,10 @@
+package com.floyd.barber;
+
+public class Eclipse {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        System.out.println("See you on the dark side of the moon");
+    }
+
+}
