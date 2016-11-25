7be10b6905561631a988f9d003a32b02aeb07b5b
vak101995 <vak101995@gmail.com>
2016-11-25 12:45:07 +0530

    remove artist name

--- Floyd/src/com/floyd/barber/Eclipse.java
+++ Floyd/src/com/floyd/barber/Eclipse.java
@@ -5,9 +5,7 @@ public class Eclipse {
     public static void main(String[] args) {
         // TODO Auto-generated method stub
         System.out.println("Album Name: Wish That you were here");
-        
-        System.out.println("Artist: Pink Floyd");
-        
+                
         System.out.println("See you on the dark side of the moon");
         
         System.out.println("Have a Cigar");
