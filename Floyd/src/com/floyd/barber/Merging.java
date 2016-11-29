af468de1f3d031f6ff23d7e8c0e52af80e2e146e
vigness <vigness@VIGNESS3.asiapacific.hpqcorp.net>
2016-11-29 16:49:33 +0530

    update 2

--- Floyd/src/com/floyd/barber/Merging.java
+++ Floyd/src/com/floyd/barber/Merging.java
@@ -4,8 +4,8 @@ public class Merging {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("hello");
-System.out.println("how are you?");
+System.out.println("iron man");
+System.out.println("is a superhero");
 	}
 
 }
